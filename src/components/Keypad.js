9f11cc171c08411b3079fdc9262d65a88452d0cc
Xennia Contreras <Xenbydesign@gmail.com>
2024-02-24 16:10:54 -0800

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react';
+import EyesOnMe from './EyesOnMe';
+
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
